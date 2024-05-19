7fd8d66f3b957fb0fbeca1d619169a74e55498cd
ocotorse <allouchhatim@gmail.com>
2024-05-19 22:00:08 +0100

    init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Card from './Components/Cards/Card'
 
 export default function Home() {
   return (
@@ -24,8 +25,11 @@ export default function Home() {
               height={24}
               priority
             />
+            <Card type="Global"/>
+            <Question question="What is the best way to learn?" response="The best way to learn is to practice"/>
           </a>
         </div>
+        {/* Yosra-mtz */}
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
