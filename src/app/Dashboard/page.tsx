87db2ff1f422e0cc683b5fe50774808895124614
yosra_mtz <y.moumtaz@mundiapolis.ma>
2024-07-11 17:47:39 +0100

    update design

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -48,7 +48,8 @@ const Home = () => {
         <div className="w-full overflow-hidden rounded-2xl text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-white to-slate-300">
           <div className="flex flex-row justify-between py-5 px-10 items-center text-graydark">
             <p>Modules</p>
-            <button className="bg-graydark px-5 dark:bg-gray-100 text-white text-sm rounded-md  gap-2 shadow-md flex">
+            <button  className="bg-graydark px-5 py-3 dark:bg-gray-100 text-white text-base rounded-md gap-2 shadow-md flex items-center justify-center hover:bg-gray-700 transition duration-300">
+
               <PlusIcon/>new Module
             </button>
           </div>
