46136ba55a1c63bd6c4deb625cb079ce07cc0402
yosra_mtz <y.moumtaz@mundiapolis.ma>
2024-07-21 23:40:29 +0100

    cin validation

--- src/app/utils/cinValidation.ts
+++ src/app/utils/cinValidation.ts
@@ -2,7 +2,7 @@ import Swal from 'sweetalert2';
 
 export const validateCINLength = (cin: string) => {
   // Expression régulière pour valider le CIN
-  const regex = /^[a-zA-Z]{1,2}\d{1,7}$/;
+  const regex = /^[a-zA-Z]{1,2}\d{4,6}$/;
 
   // Vérifiez la longueur du CIN
   if (cin.length > 8) {
@@ -19,7 +19,7 @@ export const validateCINLength = (cin: string) => {
     Swal.fire({
       icon: 'error',
       title: 'Erreur',
-      text: 'Le CIN doit commencer par une ou deux lettres suivies de chiffres.',
+      text: 'Le CIN doit commencer par une ou deux lettres suivies de 4 à 6 chiffres.',
     });
     return false;
   }
